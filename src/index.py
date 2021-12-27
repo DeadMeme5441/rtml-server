72a66bd9379d55d6c1d1c55a3d398e9ae38fed45
DeadUnderscoreMeme <deadunderscorememe@gmail.com>
2021-12-27 15:14:39 +0530

    removed cors I'm sad now

--- src/index.py
+++ src/index.py
@@ -18,18 +18,18 @@ async def hello_world():
     return {"message": "Hello World"}
 
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://13.233.94.116:3000",
-        "http://127.0.0.1:3000",
-        "http://0.0.0.0:3000",
-    ],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=[
+#         "http://localhost:3000",
+#         "http://13.233.94.116:3000",
+#         "http://127.0.0.1:3000",
+#         "http://0.0.0.0:3000",
+#     ],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 
 @app.post("/file/upload")
