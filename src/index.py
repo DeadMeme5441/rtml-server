7dead776b6bdd0d09ae2ec4beff9ae6642ca4f03
DeadUnderscoreMeme <deadunderscorememe@gmail.com>
2021-12-27 15:23:21 +0530

    * all origins

--- src/index.py
+++ src/index.py
@@ -20,12 +20,7 @@ async def hello_world():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://13.233.94.116:3000",
-        "http://127.0.0.1:3000",
-        "http://0.0.0.0:3000",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
